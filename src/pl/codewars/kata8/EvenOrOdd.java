f5455d3d00a89830adbcc4de20f33d0ffa9e8eb9
Asia <joanna.kaminska@rdgis.pl>
2018-04-02 19:44:23 +0200

    EvenOrOdd - kata 8

--- src/pl/codewars/kata8/EvenOrOdd.java
+++ src/pl/codewars/kata8/EvenOrOdd.java
@@ -2,9 +2,6 @@ package pl.codewars.kata8;
 
 public class EvenOrOdd {
     public static String even_or_odd(int number) {
-        if(number % 2 == 0){
-            return "Even";
-        }
-        return "Odd";
+        return number % 2 == 0 ? "Even" : "Odd";
     }
 }
